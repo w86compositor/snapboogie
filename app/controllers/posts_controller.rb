fad8b1b1b92ec54a6303059cfd52f3730af09862
Waldo <w86ccompositor@gmail.com>
2017-09-14 01:09:42 -0400

    Done with paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,18 @@ class PostsController < ApplicationController
 	
 	def index
 	end
-	
+
+	def new
+  	@post = Post.create(post_params)
+	end
+
+private
+
+# Use strong_parameters for attribute whitelisting
+# Be sure to update your create() and update() controller methods.
+
+	def post_params
+	  params.require(:post).permit(:picture)
+	end
+		
 end
