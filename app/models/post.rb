0510c3123337bb802be034e77ef57693fba7a11b
Waldo <w86ccompositor@gmail.com>
2017-09-14 00:53:54 -0400

    Added the paperclip gem and the migration picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+	has_attached_file :avatar, styles: { large: "600x600" medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
