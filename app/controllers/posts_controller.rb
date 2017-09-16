896b701ff288dc33e8713609742e05d880a077f5
Waldo <w86ccompositor@gmail.com>
2017-09-15 23:16:31 -0400

    Added the edit form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,30 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def new
   	@post = Post.new
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+	
 	def create
 		@post = Post.create(post_params)
 		redirect_to root_path
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to post_path(@post)
+	end
+
+	def destroy
 	end
 
 private
