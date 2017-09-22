7c29d8c75200c8576ec41522759690d1842ae6dd
Waldo <w86ccompositor@gmail.com>
2017-09-21 23:06:22 -0400

    Added some filters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :owned_post, only: [:edit, :update, :destroy]
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	before_action :authenticate_user!
 	
@@ -55,6 +56,13 @@ private
 
 	def post_params
 	  params.require(:post).permit(:picture, :caption)
+	end
+
+	def owned_post
+		unless current_user == @post.user
+		flash[:alert] = "That post doesn't belong to you!"
+		redirect_to root_path
+		end
 	end
 		
 end
